b749ad6aebb82740f730b27c2c4b4f1d45ec5155
Jayesh <patiljayesh026@gmail.com>
2023-05-28 12:49:01 +0530

    - Updated menu item card and location dropdown  -- Fixed location dropdown positioning  -- Add and remove quantity added in menu item card

--- components/MenuItemCard.tsx
+++ components/MenuItemCard.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 export default function MenuItemCard({ menuItem }) {
+  const [quantity, setQuantity] = useState(0);
+  const reduceQuantity = () => {
+    if (quantity > 0) setQuantity((quantity) => quantity - 1);
+  };
+  const increaseQuantity = () => {
+    setQuantity((quantity) => quantity + 1);
+  };
   return (
     <div className="h-36 flex flex-row bg-neutral-100 rounded-lg">
       <div className="item-menu-logo h-full w-1/5">Img</div>
@@ -7,22 +16,34 @@ export default function MenuItemCard({ menuItem }) {
           <p className="text-xl">{menuItem.name}</p>
           <p>Serves two</p>
           <p>⭐3.5</p>
-        </div>
-        <div className="flex flex-row justify-end self-end">
-            <p className="px-2">₹{menuItem.price}</p>
-            <div className="flex flex-row outline-transparent border-2 border-black mx-2 rounded-md">
-                <button className="h-full basis-1/3 bg-neutral-700">
-                    ➖
-                </button>
-                <div className="h-full basis-1/3 px-2">
-                    1
-                </div>
-                <button className="h-full basis-1/3 bg-neutral-700">
-                ➕
-                </button>
-            </div>
-          </div>
+        </div> 
       </div>
+      <div className="relative items-center flex flex-row justify-end self-end p-2">
+          <p className="px-2 text-md font-medium">₹{menuItem.price}</p>
+          {quantity ? (
+            <div className="flex flex-row border-2 border-black mx-2 rounded-md w-20 h-8">
+              <button
+                className="h-full basis-1/3 bg-neutral-700"
+                onClick={reduceQuantity}
+              >
+                ➖
+              </button>
+              <div className="h-full basis-1/3 px-2 items-center">{quantity}</div>
+              <button
+                className="h-full basis-1/3 bg-neutral-700"
+                onClick={increaseQuantity}
+              >
+                ➕
+              </button>
+            </div>
+          ) : (
+            <div className="border-2 border-black mx-2 rounded-md text-sm w-20 h-8">
+              <button className="h-full w-full font-medium" onClick={increaseQuantity}>
+                Add+
+              </button>
+            </div>
+          )}
+        </div>
     </div>
   );
 }
