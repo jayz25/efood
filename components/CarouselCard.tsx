774d875872fc9aa93c4c2901936803b94cd5f17d
Jayesh Patil <patiljayesh026@gmail.com>
2023-07-06 01:44:49 +0530

    Home page cuisines carousel update - This PR will fetch cuisines from API and display them over carousel - The purpose for this carousel is it will give user the restaurents based on selected cuisines

--- components/CarouselCard.tsx
+++ components/CarouselCard.tsx
@@ -1,8 +1,18 @@
 "use client";
+
+import Image from "next/image";
+
 export default function CarouselCard({ cardInfo }) {
+    console.log('cardIngo', cardInfo)
     return (
-        <div className="carousel-card w-48 h-48 m-4 shrink-0 shadow-lg rounded-lg bg-neutral-50">
-            {cardInfo.name}
+        <div className="flex flex-col px-3">
+            <div className="carousel-card w-48 h-48 m-4 shrink-0 shadow-lg rounded-full bg-neutral-50 overflow-hidden relative drop-shadow-2xl">
+                <Image src={cardInfo.logo_url} fill={true} alt={cardInfo.name} objectFit="cover"
+                />
+            </div>
+            <div className="flex justify-center font-medium">
+                {cardInfo.cuisine}
+            </div>
         </div>
     )
 }
